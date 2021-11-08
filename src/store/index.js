444162dbc966bf047b09dbcaf38189bde7cb3b82
Stanislaw Cruz <stanycruz@outlook.com>
2021-11-08 18:49:52 -0300

    useReducer #02

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { reducer } from './reducers'
+import { numberAdd2 } from './actions/number'
+
+const initialState = {
+    cart: [],
+    products: [],
+    user: null,
+    // foco
+    number: 0
+}
+
+export {
+    initialState,
+    reducer
+}
